8c4943b27f3b6a6efa37f5592a3b469dd137ee71
Ariday D <juan.pfaeskillsup@gmail.com>
2022-09-27 10:03:35 +0100

    reorganizacion de carpetas, creacion de archivo php

---
+++ modules/include/new_reg.php
@@ -0,0 +1,10 @@
+<?php
+require "../require/config.php";
+
+$name = $email = $phone= $street = $city = $state = $zip = $news = $opcion = $other;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+}
+
+?>
