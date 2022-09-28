a429dbec616e0cb9cbd941fa9aae2125e9c04b17
Ariday D <juan.pfaeskillsup@gmail.com>
2022-09-28 10:51:26 +0100

    Cambios en el required poniendo en si un placholder y tambien cambios en lo radio que no seleccionaba la opcion, se le añadio id a sus label para que funcionara

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -4,7 +4,10 @@ require "../require/config.php";
 $name = $email = $phone= $street = $city = $state = $zip = $news = $opcion = $other;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
+    echo "<br><strong>name post hay datos</strong><br>";
 
+}
 }
 
 ?>
