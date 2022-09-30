9f888253e7e629fb239870f76efa0922eb7c6e7a
Ariday D <juan.pfaeskillsup@gmail.com>
2022-09-30 09:33:17 +0100

    Controlar las variables que llegan

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -2,58 +2,141 @@
 require "../require/config.php";
 
 $name = $email = $phone= $street = $city = $state = $zip = $news = $opcion = $other="";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
-    echo "<br><strong>name post hay datos</strong><br>";
-    $name= $_POST["name"];
-    $email= $_POST["email"];
-    $phone= $_POST["phone"];
-    $street= $_POST["street"];
-    $city= $_POST["city"];
-    $state= $_POST["state"];
-    $zip= $_POST["zip"];
-    $news= $_POST["news"];
-    $opcion= $_POST["opcion"];
-    $other= $_POST["other"];
-
-    function limpiar_datos($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-
-    }
-    //nombre email y telefono.
-
-//revisar
-
-function validar_name($name){
-if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-    return false;
-}else{return true;
+//funciones
+/**
+* Función para limpiar un dato procedentes de un formulario.
+*
+* @param  $data
+* @return $data
+*/
+function limpiar_dato($data)
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
 }
 
-//revisaar
+// nombre, email y nº telefono.
+/**
+* Función para validar nombre que solo contenga letras min y MAY, y espacio en blanco.
+*
+* @param $name
+* @return boolean
+*/
+function validar_name($name)
+{
+if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+return false;
+} else {
+return true;
+}
+}
 
- function validar_email($email){
+/**
+* Función que valida un email
+*
+* @param $email
+* @return boolean
+*/
+function validar_email($email)
+{
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    return false;
-}else{return true;
+return false;
+} else {
+return true;
+}
 }
 
+
+// TODO: Documentar función.
+/**
+* Validar un número de telefono.
+*
+* @param $phone
+* @return Boolean
+*/
+function validar_phone($phone)
+{
+if (!preg_match('/^[0-9]{10}+$/', $phone)) {
+return false;
+} else {
+return true;
+}
+}
+
+    
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    print_r ($_POST);
+    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
+        echo "<br><strong>Metodo post enviado</strong><br>";
+        $name= limpiar_dato($_POST["name"]);
+        echo "<strong>Nombre: </strong>" . $name . "<br>";
+        $email= limpiar_dato($_POST["email"]);
+        echo "<strong>Email: </strong>" . $email . "<br>";
+        $phone= limpiar_dato($_POST["phone"]);
+        echo "<strong>Telefono: </strong>" . $phone . "<br>";
+        
+        if(isset($_POST["address"])){
+            $address= limpiar_dato($_POST["address"]);
+        }else{$address= NULL;}
+        }
+
+        $street= limpiar_dato($_POST["street"]);
+        $city= limpiar_dato($_POST["city"]);
+        $state= limpiar_dato($_POST["state"]);
+        $zip= limpiar_dato($_POST["zip"]);
+        $news= limpiar_dato($_POST["news"]);
+        $opcion= limpiar_dato($_POST["opcion"]);
+        $other= limpiar_dato($_POST["other"]);
+
+        //var_dump($newsletter);
+//echo "</br>";
+/* $newsletter = filter_input(
+INPUT_POST,
+'newsletter',
+FILTER_SANITIZE_SPECIAL_CHARS,
+FILTER_REQUIRE_ARRAY
+); */
+var_dump($newsletter);
+// echo "</br>";
+
+// === Usa un array y muestra sus valores separados por coma (o lo que se ponga entre comillas).
+/* $string=implode(", ",$newsletter);
+echo $string;
+echo "</br>"; */
+// === FIN MOSTRAR valores array.
+
+$othert = limpiar_dato($_POST["othert"]);
+echo "<strong>Noticias que quiere recibir: $newsletter";
+var_dump($name);
+
+echo "<br><strong>Name:</strong> $name <br>";
+echo "<strong>Telefono:</strong> $phone <br>";
+echo "<strong>Email: </strong> $email <br>";
+
+if (validar_name($name)){
+echo "validada";
+} else {
+echo "no valida";
+};
+/* revisar validaciones
+        if(validar_email($email)){
+            echo"validada";
+        } else{
+            echo"no valida";
+        }
+        
+        if(validar_telefono($telefono)){
+            echo"validada";
+        } else{
+            echo"no valida";
+        }
+*/
+}
+    //nombre email y telefono.
 //revisar
 
-function validar_telefono($phone){
-if (!preg_match('/^[0-9]{10}+$/',)){
-        return false;
-    }else{return true;  
-    }
 
-
-
-}
-    }
-}
-}
-}
 ?>
